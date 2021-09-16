a343a68ec9805943c205546d510a3e97a402731d
jenayjeeva99 <jenay.jeeva@connect.qut.edu.au>
2021-09-16 15:57:24 +1000

    Returns articles with only the important fields

--- apis/news.js
+++ apis/news.js
@@ -1,65 +1,50 @@
 const { default: axios } = require('axios');
 const https = require('https');
 
-// const getNewsObjects = (req, res) => {
-//     const options = createNewsOptions(req.params.title);
-//     // console.log(options);
-   
-//     const newsReq = https.request(options, (newsRes) => {
-//       let body = [];
-//       newsRes.on('data',function(chunk) {
-//           body.push(chunk);
-//       });
-      
-//       newsRes.on('end', function() {
-//           res.writeHead(newsRes.statusCode,{'content-type': 'application/json'});
-          
-//           const bodyString = body.join('');
-//         //   const rsp = JSON.parse(bodyString);
-//         //   const s = createPage('Flickr Photo Search',rsp);
-//         // console.log(JSON.parse(bodyString));
-//           res.end(bodyString);
-//       });
-      
-//     });
-   
-//     newsReq.on('error', (e) => {
-//         console.error(e.message);
-//     });
-   
-//     newsReq.end();   
-// }
 
 const getNewsObjects = (query) => {
     return axios
     .get("https://newsapi.org/v2/everything?" +"q=" + query + 
-    "&pageSize=10" + "&language=en" + "&sortBy=relevancy" + 
+    "&pageSize=5" + "&language=en" + "&sortBy=relevancy" + 
     "&apiKey=" + process.env.NEWSAPIKEY)
     .then(response => {
-        return response.data.articles;
+        return truncateArticles(response.data.articles);
     })
     .catch(error => {
         console.log(error.response.statusText);
     });
 };
 
+const truncateArticles = (articles) => {
+    truncatedArticles = [];
+    for (const article of articles) {
+        truncatedArticles.push({
+            source: article.source.name,
+            author: article.author,
+            title:  article.title,
+            url:    article.url,
+            image:  article.urlToImage
+        })
+    }
 
-const params = {
-    pageSize: 10,
-    language: 'en',
-    sortBy: 'relevancy',
-    api_key:  process.env.NEWSAPIKEY
-  };
-  
-  function createNewsOptions(query) {
-  
+    return truncatedArticles;
+}
+
+function createOptions(query) {
+    const params = {
+        pageSize: 5,
+        language: 'en',
+        sortBy: 'relevancy',
+        api_key:  process.env.NEWSAPIKEY
+    };
+    
     const options = {
         hostname: 'newsapi.org',
         port:     443,
         path:     '/v2/everything?',
         method:   'GET'
-    }
-  
+    };
+    
     const str = 'q=' + query +
                 '&pageSize='+ params.pageSize +
                 '&language='+ params.language +

c993fde1a02db33553064f6a9e76ee177364a8b0
jenayjeeva99 <jenay.jeeva@connect.qut.edu.au>
2021-09-16 16:00:39 +1000

    Creates full string of all articles' array string contents + creates 25% content summary field for all articles

--- apis/analysis.js
+++ apis/analysis.js
@@ -3,35 +3,107 @@ var axios = require("axios").default;
 var qs = require("qs");
 
 
-const postAnalysis = (url) => {
-    const options = createOptions(url);
-
-    return axios.request(options)
-    .then(response => {
-        // response.data.website.contents.forEach(paragraph => console.log(paragraph + '\n'));
-        contents = response.data.website.contents;
-        return contents;
-    })
-    .catch(error => {
-        console.log(error);
-    });
-};
-
-const createOptions = (url) => {
-    var options = {
-        method: 'POST',
-        url: 'https://text-analysis12.p.rapidapi.com/website-extraction/api/v1.3',
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'x-rapidapi-host': 'text-analysis12.p.rapidapi.com',
-            'x-rapidapi-key': 'cdd272ca75mshe121e3b556e47edp170e96jsn82669f5224d3'
-        },
-        data: qs.stringify({language: 'english', url: url})
+const createContentsPromises = (articles) => {
+  let promises = [];
+  for (const article of articles) {
+    const options = {
+      method: 'POST',
+      url: 'https://text-analysis12.p.rapidapi.com/website-extraction/api/v1.3',
+      headers: {
+        'content-type':   'application/x-www-form-urlencoded',
+        'x-rapidapi-host': 'text-analysis12.p.rapidapi.com',
+        'x-rapidapi-key': process.env.RAPIDAPIKEY
+      },
+      data: qs.stringify({language: 'english', url: article.url})
     };
-    return options;
+    promises.push(axios.request(options));
+  }
+  return promises;
 }
 
-module.exports = {postAnalysis};
+const getContents = (articles) => {
+  const promises = createContentsPromises(articles);
+  return Promise.all(promises)
+  .then( (responses) => {
+    let i = 0;
+    for (const response of responses) {
+      try {
+        const textContent = response.data.website.contents;
+        if (textContent === "") throw error;
+        articles[i].content = createFullTextString(textContent);
+        i++;
+      }
+      catch {
+        articles[i].error = "No content in this article provided";
+      }
+    }
+  })
+  .then(() => articles)
+  .catch((error) => {
+    console.log(error.response);
+  });
+}
+
+const createFullTextString = (paragraphs) => {
+  let i = 0;
+  paragraphs.forEach((paragraph) => {
+    paragraphs[i] = paragraph+' \n ';
+    i++;
+  });
+  const fulltext = paragraphs.join('');
+  return fulltext;
+};
+
+const createSummaryPromises = (articles) => {
+  let promises = [];
+
+  for (const article of articles) {
+    const options = {
+      method: 'POST',
+      url: 'https://text-analysis12.p.rapidapi.com/summarize-text/api/v1.1',
+      headers: {
+        'content-type': 'application/json',
+        'x-rapidapi-host': 'text-analysis12.p.rapidapi.com',
+        'x-rapidapi-key': process.env.RAPIDAPIKEY
+      },
+      data: {
+        language: 'english',
+        summary_percent: 25,
+        text: article.content
+      }
+    };
+    
+    promises.push(axios.request(options));
+  }
+  return promises;
+};
+
+const getSummary = (articles) => {
+  const promises = createSummaryPromises(articles);
+  return Promise.all(promises)
+  .then( (responses) => {
+    let i = 0;
+    for (const response of responses) {
+      try {
+        const summaryAnalysis = response.data.summary;
+        articles[i].summary = summaryAnalysis;
+        i++;
+      }
+      catch {
+        articles[i].error = "No sentiment could be provided";
+      }
+    }
+  })
+  .then(() => {
+    return articles;
+  })
+  .catch((error) => {
+    console.log(error.response);
+  });
+}
+
+
+module.exports = {getContents, getSummary};
     
 
 
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var https = require('https');
 var axios = require('axios');
-const {getNewsObjects} = require('../apis/news')
-const {postAnalysis} = require('../apis/analysis')
+const {getNewsObjects} = require('../apis/news');
+const {getContents, getSummary} = require('../apis/analysis');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,15 +11,22 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/search/:title', (req, res) => {
-  // getNewsObjects(req, res);
-  getNewsObjects(req.params.title)
-  .then(articles => {
-    postAnalysis(articles[0].url);
-    res.send(articles);
-  })
-  .catch(error => {
-    console.log(error.response.statusText);
-  });
+
+  getNewsObjects(req.params.title)  // gets article objects - publisher, author, url, image_url
+    .then( (articles) => {
+      return getContents(articles); // returns the article with added contents field
+    })
+    .then( (updatedArticles) => {
+      return getSummary(updatedArticles);
+    })
+    .then( (updatedArticles) => {
+      res.send(updatedArticles);
+      // res.render('index', {articles: updatedArticles});
+    })
+    .catch( (error) => {
+      console.log(error);
+    });
+  // contents.forEach(content => console.log(content));
 
 });
 

3294b33f641cf38b7d28a9a939fccf7af309d2a3
jenayjeeva99 <jenay.jeeva@connect.qut.edu.au>
2021-09-17 15:42:59 +1000

    update index router

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,29 @@
 var express = require('express');
-var router = express.Router();
-var https = require('https');
-var axios = require('axios');
+var router  = express.Router();
+var https   = require('https');
+var axios   = require('axios');
 const {getNewsObjects} = require('../apis/news');
 const {getContents, getSummary} = require('../apis/analysis');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'THIS IS THE NEW TITLE' });
+  res.render('index', {title: "TL;DR NEWS"});
 });
 
-router.get('/search/:title', (req, res) => {
-
-  getNewsObjects(req.params.title)  // gets article objects - publisher, author, url, image_url
-    .then( (articles) => {
-      return getContents(articles); // returns the article with added contents field
-    })
-    .then( (updatedArticles) => {
-      return getSummary(updatedArticles);
-    })
-    .then( (updatedArticles) => {
-      res.send(updatedArticles);
-      // res.render('index', {articles: updatedArticles});
+router.get('/search/', function(req, res) {
+  let topic   = req.query.topic;
+  let summary = req.query.contents;
+  getNewsObjects(topic)  // gets article objects - publisher, author, url, image_url
+    .then ((articles)   =>  getContents(articles))
+    .then ((articlesV2) =>  getSummary(articlesV2))
+    .then ((articlesV3) => {
+      // res.send(articlesV3);
+      res.render('news', {articles: articlesV3, summary: summary});
     })
     .catch( (error) => {
       console.log(error);
     });
   // contents.forEach(content => console.log(content));
-
 });
 
 

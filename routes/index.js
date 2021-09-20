2ab6565a29a5c6db9eb1b2c39fb4e5be5c9bf683
jenayjeeva <jenay.jeeva@connect.qut.edu.au>
2021-09-20 12:58:48 +1000

    only updates articles with summaries when requested

--- routes/index.js
+++ routes/index.js
@@ -12,15 +12,18 @@ router.get('/', function(req, res, next) {
 router.get('/search/', function(req, res) {
   let topic   = req.query.topic;
   let summary = req.query.contents;
-  getNewsObjects(topic)  // gets article objects - publisher, author, url, image_url
-    .then ((articles)   =>  getContents(articles)) // update article objects with contents field
-    .then ((articlesV2) =>  getSummary(articlesV2)) // update articles objects with summaries of contents
-    .then ((articlesV3) => {
-      // res.send(articlesV3);
-      res.render('news', {articles: articlesV3, summary: summary});
+  getNewsObjects(topic)       // gets article objects - source, author, url, image_url
+    .then ((articles)   =>  { // update article objects with text contents field
+      return getContents(articles)
+    }) 
+    .then ((articlesV2) =>  { // update articles objects with summaries of contents
+      return (summary === "summary") ? getSummary(articlesV2) : articlesV2
+    }) 
+    .then ((articlesV3) => {  // render news page
+      res.render('news', {articles: articlesV3, summary: summary}); 
     })
-    .catch( (error) => {
-      res.render('news', {error: error.message});
+    .catch( (error) => {      // render news page with error message
+      res.render('news', {error: error.message}); 
     });
 });
 
